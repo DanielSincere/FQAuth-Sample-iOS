e3b930af6cf89ae3b4af8f070949ebf8b95ee3a7
Full Queue Developer <FullQueueDeveloper@fullqueuedeveloper.com>
2023-02-01 16:06:28 -0500

    test Verify Freshly Signed Token

--- AppTests/JWTVerifierTests.swift
+++ AppTests/JWTVerifierTests.swift
@@ -1,6 +1,7 @@
 import XCTest
 @testable import App
 import Foundation
+import JWTKit
 
 final class JWTVerifierTests: XCTestCase {
 
@@ -41,5 +42,41 @@ final class JWTVerifierTests: XCTestCase {
         XCTFail("Unexpected error \(error.localizedDescription)")
       }
     }
+  }
+
+  func testVerifyFreshlySignedToken() throws {
+    let (freshlySignedToken, jwks) = try freshlySign(sample: .sample1)
+    let verifier = JWTVerifier(keySet: jwks,
+                               urlSession: FakeURLSession())
+    XCTAssertNoThrow(try verifier.verify(jwt: freshlySignedToken))
+  }
+
+  private func freshlySign(sample: FQAuthSessionTokenFixtures) throws -> (token: String, JWKS) {
+
+    let privateKeyBase64 = "LS0tLS1CRUdJTiBFQyBQQVJBTUVURVJTLS0tLS0KQmdVcmdRUUFJdz09Ci0tLS0tRU5EIEVDIFBBUkFNRVRFUlMtLS0tLQotLS0tLUJFR0lOIEVDIFBSSVZBVEUgS0VZLS0tLS0KTUlIYkFnRUJCRUVDLzFLcUxVaDgzdXZ2ZWJnS0RvWTcvaVhWdkR0dGt3b3RPRjg1ck5yMzMxNnNxbEZHVEJoagppcmx1MHhua3hqY2hEeXpVV0E0QXBvNnpYekpKV1RKTU9hQUhCZ1VyZ1FRQUk2R0JpUU9CaGdBRUFkNURXaGFLClRwT1hhK21qaXgweG8xN1NGSGx4UG16YXdKWkJiRldXQ3dZMFZOSTdveGlBcHRLNlVtbWdlM3NBVkxSMitoQTcKTGsyRm9aSEltYmlEbUdrcUFLcTdyRUNmYjB2SHRmeWhXWEdydjBRMnZ1MTV1elF5ZldkNDhSV2ZEbEVadTE2eAp0aWV1aHpnSjJGWmI2bkMvb2dFYjk5TjNxajVWT0xTeXFzcnNQejlKCi0tLS0tRU5EIEVDIFBSSVZBVEUgS0VZLS0tLS0K"
+    let signers = JWTSigners()
+
+    let unverifiedSampleToken: FQAuthSessionToken = try signers.unverified(FQAuthSessionTokenFixtures.sample1.rawValue)
+
+    let updatedSampleToken = unverifiedSampleToken.withUpdatedExpiration(newExpiration: Date(timeIntervalSinceNow: 600))
+    let privateKey = try ECDSAKey.private(pem: Data(base64Encoded: privateKeyBase64)!)
+    signers.use(.es512(key: privateKey))
+
+    let resignedToken = try signers.sign(updatedSampleToken)
+
+    let jwks = JWKS(keys: [.ecdsa(.es512,
+                                  identifier: JWKIdentifier(string: "testJWKs"),
+                                  x: privateKey.parameters?.x,
+                                  y: privateKey.parameters?.y,
+                                  curve: .p521,
+                                  privateKey: nil)])
+
+    return (resignedToken, jwks)
+  }
+}
+
+private extension FQAuthSessionToken {
+  func withUpdatedExpiration(newExpiration: Date) -> Self {
+    .init(userID: self.userID!, deviceName: self.deviceName, expiration: ExpirationClaim(value: newExpiration), iss: self.iss)
   }
 }
