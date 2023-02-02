a5e33d4dd61aabed34e8de81b5f224c6ea215632
Full Queue Developer <FullQueueDeveloper@fullqueuedeveloper.com>
2023-02-02 15:49:17 -0500

    git hub actions

--- Swish/Sources/runtests/main.swift
+++ Swish/Sources/runtests/main.swift
@@ -1,4 +1,12 @@
 import Sh
 import Foundation
 
-try sh(.terminal, "xcrun xcodebuild test -scheme App")
+try sh(.terminal,
+"""
+xcrun xcodebuild test \
+-scheme App \
+-project App.xcodeproj \
+-sdk iphoneos \
+-destination "platform=iOS Simulator,OS=16.1,name=iPhone 14 Pro Max" \
+CODE_SIGNING_ALLOWED=No
+""")
